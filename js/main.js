8b541ab3d936fb8b5c91138f7b26fa4306b66e0f
Cristian David <cristian.david909@gmail.com>
2016-01-24 18:34:33 -0500

    Added material design outline

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 
+// global DOM Objects
 var objMsgButton = $("#submitMsgButton");
 var objEnteredText = $("#enteredText");
 var objChatText = $("#chatText");
+var userName = "Username";
 
+// global objects
+/*var msgPacket = {
+  sender: 
+}*/
 
 window.onload = function() {
   
@@ -12,20 +18,70 @@ window.onload = function() {
 $(objEnteredText).keyup(function (e) {
   var keycode = (event.keyCode ? event.keyCode : event.which);
   if(keycode === 13){
-    addMsgToChat();
+    createMsgPacket();
   }
 });
 objMsgButton.click(function(){
-  addMsgToChat();
+  createMsgPacket();
 });
 
-var addMsgToChat = function(){
-  var chatDisplayText = objChatText.val();
-  var enteredText = objEnteredText.val();
+var createMsgPacket = function(){
   
-  objChatText.val( chatDisplayText ? chatDisplayText+"\n"+enteredText : enteredText );
+  // RUN MSG PROCESSING
+  // SEND TO SERVER
   
-  objChatText.animate({ scrollTop: objChatText.prop("scrollHeight")}, 250);
+  // RETRIEVE RESPONSE
   
-  objEnteredText.val("");
+  // push to chat
+  addMsgToChat("user");
+}
+
+var addMsgToChat = function(senderType){
+  
+  var chatDisplayText = objChatText.text();
+  var enteredText = objEnteredText.val().replace(/(\r\n|\n|\r)/gm,"");
+  var msgClass = "";
+  
+  var errorMsg = "";
+  
+  
+  switch(senderType){
+    case "user":
+      msgClass = "myMsg";
+      break;
+    case "server":
+      msgClass = "publicMsg";
+      break;
+    default:
+      msgClass = "errorMsg";
+  }
+
+  // Error checking
+  if ( !enteredText ) {
+    errorMsg += "No text entered.";
+  }
+
+  if( !errorMsg ) {
+    // Create card object that contains the message and add it to the message display card
+    var dt = new Date();
+    var time = dt.getHours()%12 + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+    
+    objChatText.append('<div class="row">'+
+                        '<div class="col s12">'+
+                          '<div class="card messageCard '+msgClass+'">'+
+                            '<div class="card-content" value="">'+
+                              enteredText+
+                            '</div>'+
+                            '<div class="card-action extraInfo">'+
+                              userName+" - "+time+
+                            '</div>'+
+                          '</div>'+
+                        '</div>'+
+                      '</div>')
+    // End card creation
+    
+    objChatText.animate({ scrollTop: objChatText.prop("scrollHeight")}, 250);
+    
+    objEnteredText.val("");
+  }
 }
